bc1ebbe795cb28cfffa3b71b5843cc5695ff6c94
crossedbot <59076483+crossedbot@users.noreply.github.com>
2022-08-01 21:46:51 -0400

    documentation and comments

--- queue/pq.go
+++ queue/pq.go
@@ -5,20 +5,28 @@ import (
 	"time"
 )
 
+// pqItem represents a priority queue item
 type pqItem struct {
-	Exp   time.Time
-	Index int
-	Key   string
-	Value interface{}
+	Exp   time.Time   // expiration time
+	Index int         // queue index
+	Key   string      // item key
+	Value interface{} // item value
 }
 
+// pqHeap implements a prioritized "mini-heap". Where a heap is a tree with the
+// property that each node is the minimum-valued node in its subtree; see the
+// container/heap package for more information
 type pqHeap []*pqItem
 
+// Len returns the length of the heap
 func (h *pqHeap) Len() int {
 	c := *h
 	return len(c)
 }
 
+// Less returns true if the item at index i will expired before the item at
+// index j. If any item is nil, it is considered "lesser" than the other item
+// and true/false is returned for index i/j respectively
 func (h *pqHeap) Less(i, j int) bool {
 	// Put NULL values at the start of the queue to be removed upon POP
 	c := *h
@@ -34,6 +42,7 @@ func (h *pqHeap) Less(i, j int) bool {
 	return a.Exp.Before(b.Exp)
 }
 
+// Push pushes and appends the given item at the end of the prioritized heap
 func (h *pqHeap) Push(x interface{}) {
 	if item, ok := x.(*pqItem); ok {
 		item.Index = h.Len()
@@ -41,6 +50,8 @@ func (h *pqHeap) Push(x interface{}) {
 	}
 }
 
+// Pop returns and removes the item at index len(heap)-1; IE. the item at the
+// end of the heap
 func (h *pqHeap) Pop() interface{} {
 	c := *h
 	l := len(c)
@@ -53,6 +64,7 @@ func (h *pqHeap) Pop() interface{} {
 	return item
 }
 
+// Swap swaps the position of the items at index i and j
 func (h *pqHeap) Swap(i, j int) {
 	c := *h
 	c[i], c[j] = c[j], c[i]
@@ -61,20 +73,34 @@ func (h *pqHeap) Swap(i, j int) {
 	*h = c
 }
 
+// PriorityQueue represents an interface to a priority queue for items with a
+// Time-To-Live (TTL)
 type PriorityQueue interface {
+	// Add adds an item to the priority queue with the given value and TTL
 	Add(key string, value interface{}, ttl time.Duration)
+
+	// Delete deletes the item with the given key from the priority queue
 	Delete(key string)
+
+	// DeleteExpired deletes all items from the priority queue that have
+	// expired based on their TTL
 	DeleteExpired(t time.Time) int
+
+	// Get returns the item for a given key and increases its TTL on
+	// retrieval
 	Get(key string, ttl time.Duration) interface{}
+
+	// Len returns the length of the priority queue
 	Len() int
 }
 
+// pq implements a PriorityQueue for items with expiration times
 type pq struct {
-	Heap *pqHeap
-	// XXX we could use an unsigned integer but strings are more versatile
-	Map map[string]*pqItem
+	Heap *pqHeap            // The priority queue heap
+	Map  map[string]*pqItem // Map of priority queue items and their keys
 }
 
+// NewPriorityQueue returns a new PriorityQueue
 func NewPriorityQueue() PriorityQueue {
 	return &pq{
 		Heap: new(pqHeap),
--- queue/queue.go
+++ queue/queue.go
@@ -2,18 +2,24 @@ package queue
 
 import ()
 
+// Queue represents an interface to queue collection
 type Queue interface {
+	// Len returns the length of the queue
 	Len() int
 
+	// Pop returns and removes the next item in the queue
 	Pop() interface{}
 
+	// Push pushes a new item onto the queue
 	Push(value interface{})
 }
 
+// queue implements the Queue collection
 type queue struct {
 	data []interface{}
 }
 
+// New returns a new Queue
 func New() Queue {
 	return &queue{}
 }
